f441607ccd8d625b60cfb2303b3703b32c0dc6f1
aniketshrestha04 <aniket.shrestha12@gmail.com>
2026-08-24 21:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const priceElements = document.querySelectorAll('.price');
+	let total = 0;
+
+	priceElements.forEach(item => {
+		total += parseInt(item.textContent, 10);
+	});
+
+	const table = document.querySelector('table');
+
+	const newRow = document.createElement('tr');
+	const newCell = document.createElement('td');
+
+	newCell.textContent = total;
+
+	newRow.appendChild(newCell);
+	table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
